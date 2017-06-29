00e86b2c71292413e57c7b2bfb9e2680f02f09f3
harinath <coupondunia@CouponDunias-MacBook-Pro-2.local>
2017-06-29 17:55:13 +0530

    added static to settings.py

--- elections/settings.py
+++ elections/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = ( 
+            os.path.join(PROJECT_ROOT, 'static'),
+            )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
