a1010511b910c151a288592f971a108512b9ddeb
harinath <coupondunia@CouponDunias-MacBook-Pro-2.local>
2017-06-29 22:16:51 +0530

    response made json

--- polling/views.py
+++ polling/views.py
@@ -3,18 +3,17 @@ from __future__ import unicode_literals
 from .models import Results
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 def get_value(request):
     key=request.GET.get('id','')
     record=Results.objects.get(id=key)
-    list=[]
-    list.append(record.constituency)
-    list.append(record.constituency_code)
-    list.append(record.leading_candidate)
-    list.append(record.leading_party)
-    list.append(record.trailing_candidate)
-    list.append(record.trailing_party)
-
-    return HttpResponse(list)
+    final_dict = {}
+    final_dict["constituency"] = record.constituency
+    final_dict["constituency_code"] = record.constituency_code
+    final_dict["leading_party"] = record.leading_party
+    final_dict["trailing_candidate"] = record.trailing_candidate
+    final_dict["trailing_party"] = record.trailing_party
+    return HttpResponse(json.dumps(final_dict), content_type="application/json")
 
 # Create your views here.
